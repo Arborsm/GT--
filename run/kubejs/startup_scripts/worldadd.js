61547631548ea5c309978fe36f8c2856291526dc
Arbor <44860537+Arborsm@users.noreply.github.com>
2023-11-05 02:23:34 +0800

    Delete run directory

--- run/kubejs/startup_scripts/worldadd.js
+++
@@ -1,9 +0,0 @@
-GTCEuStartupEvents.registry('gtceu:world_gen_layer', event => {
-  event.create('ad')
-    .target(['ad_astra:moon_stone', 'ad_astra:venus_stone', 'ad_astra:mars_stone', 'minecraft:blackstone', 'minecraft:basalt', 'ad_astra:mercury_stone', 'minecraft:deepslate', 'ad_astra:glacio_stone'])
-    .addDimension('ad_astra:moon')
-    .addDimension('ad_astra:venus')
-    .addDimension('ad_astra:mars')
-    .addDimension('ad_astra:mercury')
-    .addDimension('ad_astra:glacio')
-})
