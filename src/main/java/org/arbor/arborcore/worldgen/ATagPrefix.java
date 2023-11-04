e1ba39bbeee6afe4271594b59b250634e13248d4
Arborsm <BlueGrayx@qq.com>
2023-11-05 02:15:51 +0800

    Register ore tagprefix.

--- src/main/java/org/arbor/arborcore/worldgen/ATagPrefix.java
+++ src/main/java/org/arbor/arborcore/worldgen/ATagPrefix.java
@@ -9,6 +9,7 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.material.MapColor;
 
+@SuppressWarnings("unused")
 public class ATagPrefix{
     public static final TagPrefix oreMoonStone = new TagPrefix("moon_stone")
             .langValue("MoonStone %s Ore")
