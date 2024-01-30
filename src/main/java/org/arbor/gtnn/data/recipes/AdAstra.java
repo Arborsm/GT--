8159312bbea5d74bd23cf826fcf35705fef9125d
Arborsm <BlueGrayx@qq.com>
2024-01-30 13:31:21 +0800

    fk adastra

---
+++ src/main/java/org/arbor/gtnn/data/recipes/AdAstra.java
@@ -0,0 +1,11 @@
+package org.arbor.gtnn.data.recipes;
+
+import net.minecraft.data.recipes.FinishedRecipe;
+
+import java.util.function.Consumer;
+
+public class AdAstra {
+    public static void init(Consumer<FinishedRecipe> provider) {
+
+    }
+}
