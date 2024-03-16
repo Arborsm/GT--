3630fccb839a8315d73083451b9709e2f34d83e7
Arborsm <BlueGrayx@qq.com>
2024-03-16 23:39:34 +0800

    fixed: recipe filling

---
+++ src/main/java/org/arbor/gtnn/mixin/emi/EmiRecipeHandlerMixin.java
@@ -0,0 +1,29 @@
+package org.arbor.gtnn.mixin.emi;
+
+import com.lowdragmc.lowdraglib.gui.modular.ModularUIContainer;
+import dev.emi.emi.api.recipe.handler.EmiRecipeHandler;
+import dev.emi.emi.registry.EmiRecipeFiller;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.InventoryMenu;
+import org.arbor.gtnn.GTNN;
+import org.arbor.gtnn.emi.NGTEmiRecipeHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(EmiRecipeFiller.class)
+public class EmiRecipeHandlerMixin {
+    @SuppressWarnings("unchecked")
+    @Inject(method = "getAllHandlers", at = @At("HEAD"), cancellable = true, remap = false)
+    private static <T extends AbstractContainerMenu> void AddGTEmiRecipeHandler(AbstractContainerScreen<T> screen, CallbackInfoReturnable<List<EmiRecipeHandler<T>>> cir) {
+        if (GTNN.getClientConfig().enableRemakeGTMEMI && screen != null && !(screen.getMenu() instanceof InventoryMenu)) {
+            if (screen.getMenu() instanceof ModularUIContainer) {
+                cir.setReturnValue((List<EmiRecipeHandler<T>>) (List<?>) List.of(new NGTEmiRecipeHandler()));
+            }
+        }
+    }
+}
