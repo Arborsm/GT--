5d125073e7b70015bdf719eb1268dc586684bea3
Arborsm <BlueGrayx@qq.com>
2024-03-22 14:08:52 +0800

    hotfix AE2 amd EMI

--- src/main/java/org/arbor/gtnn/mixin/gt/Ae2PatternTerminalHandlerMixin.java
+++ src/main/java/org/arbor/gtnn/mixin/gt/Ae2PatternTerminalHandlerMixin.java
@@ -2,6 +2,7 @@ package org.arbor.gtnn.mixin.gt;
 
 import com.gregtechceu.gtceu.integration.emi.recipe.Ae2PatternTerminalHandler;
 import dev.emi.emi.api.recipe.EmiRecipe;
+import org.arbor.gtnn.emi.NGTEmiRecipe;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class Ae2PatternTerminalHandlerMixin {
     @Inject(method = "supportsRecipe", at = @At("HEAD"), cancellable = true, remap = false)
     private void whyGTMStillNotUpdate(EmiRecipe recipe, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(true);
+        cir.setReturnValue(recipe instanceof NGTEmiRecipe);
     }
 }
