55e0faac3b02f42c12443eb0870b8f79d24bc8a3
Igneaalis <7469854+Igneaalis@users.noreply.github.com>
2025-09-27 18:15:06 +0300

    TagPrefix: BotaniaMaterials

--- src/main/kotlin/dev/arbor/gtnn/data/materials/BotaniaMaterialsModification.kt
+++ src/main/kotlin/dev/arbor/gtnn/data/materials/BotaniaMaterialsModification.kt
@@ -7,16 +7,16 @@ import vazkii.botania.common.item.BotaniaItems
 
 object BotaniaMaterialsModification {
     fun init() {
-        TagPrefix.ingot.setIgnored(GTNNMaterials.TerraSteel, BotaniaItems.terrasteel)
-//        TagPrefix.block.setIgnoredBlock(GTNNMaterials.TerraSteel, BotaniaBlocks.terrasteelBlock)
-//        TagPrefix.nugget.setIgnored(GTNNMaterials.TerraSteel, BotaniaItems.terrasteelNugget)
-//
-//        TagPrefix.ingot.setIgnored(GTNNMaterials.ManaSteel, BotaniaItems.manaSteel)
-//        TagPrefix.block.setIgnoredBlock(GTNNMaterials.ManaSteel, BotaniaBlocks.manasteelBlock)
-//        TagPrefix.nugget.setIgnored(GTNNMaterials.ManaSteel, BotaniaItems.manasteelNugget)
-//
-//        TagPrefix.ingot.setIgnored(GTNNMaterials.Elementium, BotaniaItems.elementium)
-//        TagPrefix.block.setIgnoredBlock(GTNNMaterials.Elementium, BotaniaBlocks.elementiumBlock)
-//        TagPrefix.nugget.setIgnored(GTNNMaterials.Elementium, BotaniaItems.elementiumNugget)
+        TagPrefix.ingot.setIgnored(GTNNMaterials.TerraSteel, { BotaniaItems.terrasteel })
+        TagPrefix.block.setIgnoredBlock(GTNNMaterials.TerraSteel, BotaniaBlocks.terrasteelBlock)
+        TagPrefix.nugget.setIgnored(GTNNMaterials.TerraSteel, { BotaniaItems.terrasteelNugget })
+
+        TagPrefix.ingot.setIgnored(GTNNMaterials.ManaSteel, { BotaniaItems.manaSteel })
+        TagPrefix.block.setIgnoredBlock(GTNNMaterials.ManaSteel, BotaniaBlocks.manasteelBlock)
+        TagPrefix.nugget.setIgnored(GTNNMaterials.ManaSteel, { BotaniaItems.manasteelNugget })
+
+        TagPrefix.ingot.setIgnored(GTNNMaterials.Elementium, { BotaniaItems.elementium })
+        TagPrefix.block.setIgnoredBlock(GTNNMaterials.Elementium, BotaniaBlocks.elementiumBlock)
+        TagPrefix.nugget.setIgnored(GTNNMaterials.Elementium, { BotaniaItems.elementiumNugget })
     }
 }
